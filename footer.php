8353eb3ed8061cef606bc8ba1af01951ef147170
Jainesh01 <103590021+Jainesh01@users.noreply.github.com>
2022-04-13 21:05:42 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,63 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p>This system is being developed to notify the family member's the health status of the patient admitted in the hospital.
+We'll try to keep them updated every hour via e-mail or sms that they have provided during admitting the patient into the hospital.			</p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>Contact Us</h6>
+            <p> Ahmedabad City</p>
+            <ul>
+              <li class="mobile">
+                <p>7016438656</p>
+                <p>8801170009-HMS</p>
+              </li>
+              <li class="email">
+                <p>hnotifier@gmail.com</p>
+                <p></p></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+
+  <?php include 'header.php';?>
+  <!-- Bnr Header -->
+  <section class="main-banner">
+    <div class="tp-banner-container">
+      <div class="tp-banner">
+        <ul>
+          
+          <!-- SLIDE  -->
+          <li > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms.jpg"  > 
+            
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-120" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:50px; font-weight:500; color:black;  max-width: auto; max-height: auto; white-space: nowrap;">Health Notifier System</div>
+            
+            
+            
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+   
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
